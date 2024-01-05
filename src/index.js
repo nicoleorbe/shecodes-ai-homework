1014dda62f5dcc857352c8bbf233fb1762512779
Nicole Orbe <109162498+nicoleorbe@users.noreply.github.com>
2024-01-05 16:22:49 -0600

    added formatting to API response for poems

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 let apiKey = "61o493tb34bd0f7a45117a0fe312a138";
-let context = "You are an expert in poetry about love and marriage.";
+let context =
+  "You are an expert in poetry about love and marriage. You only write short poems.";
 let buttonElement = document.querySelector("#submit");
 let poemContainer = document.querySelector("#poem");
 
 function showAnswer(response) {
-  console.log(response);
+  let poemResponse = response.data.answer;
 
-  document.getElementById("poem").innerHTML = `<p>${response.data.answer}</p>`;
+  let splitPoem = poemResponse.split("\n");
+
+  let typewriter = new Typewriter("#poem", {
+    loop: false,
+    delay: 50,
+  });
+
+  splitPoem.forEach((line, index) => {
+    typewriter.typeString(line);
+    if (index < splitPoem.length - 1) {
+      // If not the last line, add a break and pause
+      typewriter
+        .pauseFor(500) // pause duration
+        .typeString("<br>");
+    }
+  });
+
+  typewriter.start();
 }
 
 function handleClick(event) {
   event.preventDefault();
   poemContainer.classList.add("poem");
-  document.getElementById("poem").innerHTML = "Please wait, I'm thinking...";
+  document.getElementById("poem").innerHTML =
+    "Please wait, I'm writing something beautiful ...";
   let prompt = document.getElementById("prompt").value;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt="${prompt}"&context="${context}"&key=${apiKey}`;
   axios.get(apiUrl).then(showAnswer);
